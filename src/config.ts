bb2350732457b11375621a0ccb3b15d0e3dcaed0
Momoyeyu <momoyeyu@outlook.com>
2026-05-22 09:53:50 +0800

    Site updated: 2026-05-22 09:53:50

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: '/img/resources/banner.jpg',
+		src: '/img/resources/banner.webp',
 		position: "center",
 		credit: {
 			enable: false,
